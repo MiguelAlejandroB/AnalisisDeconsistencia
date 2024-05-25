ce4a3da9becee9a88a88e1ad21581476c38be670
MiguelAlejandroB <mibermudezc@unal.edu.co>
2024-05-25 17:38:02 -0500

    Actualizar

--- Analisis.py
+++ Analisis.py
@@ -3,26 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 #Leer el csv
-datos = pd.read_csv('caudal.csv', parse_dates=['fecha'])
+data = pd.read_csv('Alone.csv', parse_dates=['Fecha'],dayfirst=True)
 #Datos acumulados
-data['caudal_acumulado'] = data['caudal'].cumsum()
+data['caudal_acumulado'] = data['Valor'].cumsum()
 #Calcular estadisticas
-media_caudal = data['caudal_acumulado'.mean()
+media_caudal = data['caudal_acumulado'].mean()
 desviacion_estandar = data['caudal_acumulado'].std()
+RangoInferior = -desviacion_estandar
+RangoSuperior = desviacion_estandar
 #Regresión lineal
 slope, intercept, r_value, p_value, std_err = linregress(np.arange(len(data)), data['caudal_acumulado'])
-
 # Calcular la pendiente y la desviación estándar
-pendiente = slope
 
-if abs(pendiente) > desviacion_estandar:
+
+if RangoInferior <= slope <= RangoSuperior:
     print("La pendiente es mayor que la desviación estándar, los datos pueden no ser consistentes.")
 else:
     print("La pendiente es menor o igual a la desviación estándar, los datos son consistentes.")
-
+print(slope)
+print(RangoInferior)
+print(RangoSuperior)
 #  Visualizar los datos
 plt.figure(figsize=(10, 6))
-plt.plot(data['fecha'], data['caudal_acumulado'], label='Caudal')
+plt.plot(data['Fecha'], data['caudal_acumulado'], label='Caudal')
 plt.xlabel('Fecha')
 plt.ylabel('Caudal')
 plt.title('Datos de Caudal')
